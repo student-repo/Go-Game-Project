401ebc7b8f2aaebbb87432967d0162a91ea79d50
student-repo <repostudent@gmail.com>
2016-12-07 00:21:26 +0100

    more clear code

--- GoGame/src/main/java/gogame/game/engine/GameBoard.java
+++ GoGame/src/main/java/gogame/game/engine/GameBoard.java
@@ -23,52 +23,6 @@ public class GameBoard {
 	}
 	public static void main(String[] args){
 		GameBoard g = new GameBoard();
-		g.placeStone(new Point(1,3),BoardFieldOwnership.BLACK);
-		g.placeStone(new Point(3,2),BoardFieldOwnership.WHITE);
-        g.placeStone(new Point(2,2),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(2,3),BoardFieldOwnership.WHITE);
-        g.placeStone(new Point(3,1),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(3,4),BoardFieldOwnership.WHITE);
-        g.placeStone(new Point(4,2),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(4,4),BoardFieldOwnership.WHITE);
-        g.placeStone(new Point(4,3),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(5,4),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(4,5),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(3,5),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(4,2),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(2,4),BoardFieldOwnership.BLACK);
-        g.placeStone(new Point(3,3),BoardFieldOwnership.BLACK);
-
-
-
-		System.out.println("amount white groups: " + whiteGroups.size());
-		System.out.println("amount black groups: " + blackGroups.size());
-		for(FieldGroup f: blackGroups){
-			System.out.println(f.fieldsInGroup.toString().replaceAll("java.awt.Point", "") + " amount group: " + f.fieldsInGroup.size());
-			System.out.println(f.fieldsToKillThisGroup.toString().replaceAll("java.awt.Point", "") + " amount to kill: " + f.fieldsToKillThisGroup.size());
-		}
-
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		for(FieldGroup f: whiteGroups){
-			System.out.println(f.fieldsInGroup.toString().replaceAll("java.awt.Point", "") + " amount group: " + f.fieldsInGroup.size());
-			System.out.println(f.fieldsToKillThisGroup.toString().replaceAll("java.awt.Point", "") + " amount to kill: " + f.fieldsToKillThisGroup.size());
-		}
-
-//		for(Point p: boardFields.keySet()){
-//			if(boardFields.get(p) != BoardFieldOwnership.FREE){
-//				System.out.println(p.toString().replace("java.awt.Point", "") + "  " + boardFields.get(p));
-//			}
-//		}
-
-
-//		for(Point h: boardFields.keySet()){
-//
-//			if(boardFields.get(h).equals(BoardFieldOwnership.BLACK)){
-//				System.out.println(h);
-//			}
-//		}
 
 	}
 
@@ -83,29 +37,12 @@ public class GameBoard {
 			return false;
 		}
 		if (player == BoardFieldOwnership.BLACK) {
-            fofo(blackGroups, whiteGroups, point, "blackMove");
+            placeConcreteStone(blackGroups, whiteGroups, point, "blackMove");
 		}
 		else {
-            fofo(whiteGroups, blackGroups, point, "whiteMove");
+            placeConcreteStone(whiteGroups, blackGroups, point, "whiteMove");
 		}
 		return true;
-	}
-
-	private void clearFields(FieldGroup grp) {
-		HashSet<Point> points;
-		points = grp.getPoints();
-		for (Point pt : points) {
-			this.boardFields.put(pt, BoardFieldOwnership.FREE);
-		}
-
-	}
-
-	/**
-	 * Returns current board size
-	 * @return board size as Integer
-	 */
-	public int getBoardSize() {
-		return boardFields.size();
 	}
 
 	/**
@@ -118,26 +55,6 @@ public class GameBoard {
 			return true;
 		else
 			return false;
-	}
-
-	private ArrayList<FieldGroup> getNearbyWhiteGroups(Point point) {
-		ArrayList<FieldGroup> nearbyGroups;
-		nearbyGroups = new ArrayList<FieldGroup>();
-		for (FieldGroup gr : whiteGroups) {
-			if (gr.isNextTo(point))
-				nearbyGroups.add(gr);
-		}
-		return nearbyGroups;
-	}
-
-	private ArrayList<FieldGroup> getNearbyBlackGroups(Point point) {
-		ArrayList<FieldGroup> nearbyGroups;
-		nearbyGroups = new ArrayList<FieldGroup>();
-		for (FieldGroup gr : blackGroups) {
-			if (gr.isNextTo(point))
-				nearbyGroups.add(gr);
-		}
-		return nearbyGroups;
 	}
 
 	private ArrayList<Integer> getNearbyGroupsIndexes(Point point, ArrayList<FieldGroup> groups) {
@@ -191,8 +108,19 @@ public class GameBoard {
         }
     }
 
-//	private HashMap<String, ArrayList<FieldGroup> > fofo(ArrayList<FieldGroup> ownGroup,ArrayList<FieldGroup> foreignGroup, Point point){
-private void fofo(ArrayList<FieldGroup> ownGroup,ArrayList<FieldGroup> foreignGroup, Point point, String art){
+    private void updateGroupAfterKillOpponent(ArrayList<FieldGroup> ownGroup,ArrayList<FieldGroup> foreignGroup, int i){
+        for(Point o: foreignGroup.get(i).fieldsInGroup){
+            boardFields.put(o,BoardFieldOwnership.FREE);
+            for(int k = 0; k < ownGroup.size(); k++){
+                if(setContainsNearbyPoint(ownGroup.get(k).fieldsInGroup, o )){
+                    ownGroup.get(k).fieldsToKillThisGroup.add(o);
+                }
+            }
+        }
+    }
+
+    //this function uses references
+private void placeConcreteStone(ArrayList<FieldGroup> ownGroup,ArrayList<FieldGroup> foreignGroup, Point point, String art){
 
     ArrayList<Integer> nearbyGroupsIndexes = getNearbyGroupsIndexes(point, ownGroup);
     if(art.equals("blackMove")){
@@ -203,20 +131,13 @@ private void fofo(ArrayList<FieldGroup> ownGroup,ArrayList<FieldGroup> foreignGr
     }
 
             ArrayList<Integer> toRemoveGroupsIndexes = new ArrayList<>();
-            for(int i=0; i< foreignGroup.size(); i++){
+            for(int i = 0; i < foreignGroup.size(); i++){
                 if(foreignGroup.get(i).fieldsToKillThisGroup.contains(point)){
                     foreignGroup.get(i).fieldsToKillThisGroup.remove(point);
                 }
                 if(foreignGroup.get(i).fieldsToKillThisGroup.size() == 0){
                     toRemoveGroupsIndexes.add(i);
-                    for(Point o: foreignGroup.get(i).fieldsInGroup){
-                        boardFields.put(o,BoardFieldOwnership.FREE);
-                        for(int k = 0; k < ownGroup.size(); k++){
-                            if(setContainsNearbyPoint(ownGroup.get(k).fieldsInGroup, o )){
-                                ownGroup.get(k).fieldsToKillThisGroup.add(o);
-                            }
-                        }
-                    }
+                    updateGroupAfterKillOpponent(ownGroup, foreignGroup, i);
                 }
             }
             if(art.equals("blackMove")){
@@ -225,6 +146,5 @@ private void fofo(ArrayList<FieldGroup> ownGroup,ArrayList<FieldGroup> foreignGr
             else{
                 removeAllBlackGroups(toRemoveGroupsIndexes);
             }
-
     }
 }
